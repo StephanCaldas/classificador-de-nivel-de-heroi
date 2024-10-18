36f8eda7578d992b9617df96721d05795da760eb
Stephan <Stephan.caldas@gmail.com>
2024-10-18 13:45:15 -0300

    Finalizando o desafio

--- src/index.js
+++ src/index.js
@@ -1,2 +1,26 @@
-let nome = "Felipe";
-console.log(nome);
+const nome = "Troll";
+const xp = 1;
+let nivel;
+
+console.log(`Digite o nome do Herói: ${nome}.`);
+console.log(`Digite o xp do Herói: ${xp}.`);
+
+if(xp <= 1000) {
+    nivel = "Ferro";
+}else if(xp > 1000 && xp <= 2000) {
+    nivel = "Bronze";
+}else if(xp > 2000 && xp <= 5000) {
+    nivel = "Prata";
+}else if(xp > 5000 && xp <= 7000) {
+    nivel = "Ouro";
+}else if(xp > 7000 && xp <= 8000) {
+    nivel = "Platina";
+}else if(xp > 8000 && xp <= 9000) {
+    nivel = "Ascendente";
+}else if(xp > 9000 && xp <= 10000) {
+    nivel = "Imortal";
+}else{
+    nivel = "Radiante";
+}
+    
+console.log(`O Herói de nome ${nome} está no nível ${nivel}.`);
